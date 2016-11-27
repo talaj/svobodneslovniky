c78f04b2ae66df97070af5c0dea9b87465307899
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2016-11-27 14:51:21 +0100

    todo

--- en-cs/gen.py
+++ en-cs/gen.py
@@ -6,7 +6,8 @@ column_count = 5
 line_number = 0
 last_pre = None
 pre_file = None
-index = open('dict/index', 'w')
+index = open('dict/index.js', 'w')
+index.write('var index = {\n')
 
 for line in sys.stdin:
     line_number += 1
@@ -21,13 +22,14 @@ for line in sys.stdin:
             pre_file.close()
         name = '.'.join(map(str, map(ord, pre)))
         pre_file = open('dict/' + name, 'w')
-        index.write(pre + '\t' + name + '\n')
+        index.write('"{}": "{}",\n'.format(pre, name))
         last_pre = pre
     pre_file.write('\t'.join(entry[0:2]))
     pre_file.write('\n')
 
 if pre_file:
     pre_file.close()
+index.write('};\n')
 index.close()
 
 
