3f1d73f9ed75b095796ed21231f83755b12896a9
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2018-12-15 07:48:49 +0000

    w

--- en-cs/keys.py
+++ en-cs/keys.py
@@ -27,13 +27,12 @@ chunks = [entries[i * chunk_size:i * chunk_size + chunk_size]
 
 index = 0
 for chunk in chunks:
-    key = chunk[0][0][0]
-    key_encoded = base64.standard_b64encode(key.encode()).decode()
-    with open("chunks/{}".format(key_encoded), "w") as f:
+    with open("chunks/{:03}".format(index), "w") as f:
         for group in chunk:
             for entry in group:
                 f.write('\t'.join(entry))
                 f.write('\n')
+    index += 1
 
 
 #for entry in entries:
