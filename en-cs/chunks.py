e804e02af66c77477773214355734d290e703f91
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2018-12-16 15:39:58 +0000

    w

--- en-cs/chunks.py
+++ en-cs/chunks.py
@@ -3,11 +3,17 @@
 import math
 import svobodneslovniky
 
-entries = list(svobodneslovniky.read(open("en-cs.txt")))
+groups = []
+
+for entry in svobodneslovniky.read(open("en-cs.txt")):
+    key = entry[0]
+    if not groups or groups[-1][0][0] != key:
+        groups.append([])
+    groups[-1].append(entry)
 
 chunk_size = 500
-chunks = [entries[i * chunk_size:i * chunk_size + chunk_size]
-    for i in range(math.ceil(len(entries) / chunk_size))]
+chunks = [groups[i * chunk_size:i * chunk_size + chunk_size]
+    for i in range(math.ceil(len(groups) / chunk_size))]
 
 with open("chunks/index", "w") as f:
     keys = [chunk[0][0][0] for chunk in chunks]
--- en-cs/search.py
+++ en-cs/search.py
@@ -1,39 +1,16 @@
 #!/usr/bin/env python
 
 import sys
-import math
+import bisect
 
-column_count = 5
-line_number = 0
-entries = []
+index = []
+with open("chunks/index") as f:
+    for line in f:
+        index.append(line.rstrip('\n'))
 
-for line in open("en-cs.txt"):
-    line_number += 1
-    entry = line.rstrip('\n').split('\t')
-    if len(entry) != column_count:
-        raise RuntimeError('Format error on line {}: "{}"'.format(line_number, line))
-    key = entry[0]
-    if not entries or entries[-1][0][0] != key:
-        entries.append([])
-    entries[-1].append(entry)
+key = sys.argv[1]
 
-chunk_size = 500
-chunks = [entries[i * chunk_size:i * chunk_size + chunk_size]
-    for i in range(math.ceil(len(entries) / chunk_size))]
+print(index)
 
-with open("chunks/index", "w") as f:
-    keys = [chunk[0][0][0] for chunk in chunks]
-    f.write('\n'.join(keys))
-
-index = 0
-for chunk in chunks:
-    with open("chunks/{:03}".format(index), "w") as f:
-        for group in chunk:
-            for entry in group:
-                f.write('\t'.join(entry))
-                f.write('\n')
-    index += 1
-
-
-#for entry in entries:
-    #print('\t'.join(entry))
+i = bisect.bisect_left(index, key)
+print(i, index[i])
