082d7ac073c329cd9ada2308acad1986f0c20364
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2018-12-16 15:12:24 +0000

    w

--- en-cs/svobodneslovniky.py
+++ en-cs/svobodneslovniky.py
@@ -8,7 +8,7 @@ def check(entry, line, line_number):
             line_number, line))
     if entry != list(map(str.strip, entry)):
         raise RuntimeError('Unnecessary whitespace characters on line {}:'
-            ' "{}"'.format(line_number, line))
+                           ' "{}"'.format(line_number, line))
     return entry
 
 
@@ -27,12 +27,17 @@ def write(entries, file_obj):
 
 
 def sort(entries):
-    cs_sort_key = icu.Collator.createInstance(icu.Locale('cs_CZ.UTF-8')).getSortKey
-    en_sort_key = icu.Collator.createInstance(icu.Locale('en_US.UTF-8')).getSortKey
-    sort_key = lambda entry: (
-        en_sort_key(entry[0]),
-        cs_sort_key(entry[1]),
-        cs_sort_key(entry[2]),
-        cs_sort_key(entry[3]),
-        cs_sort_key(entry[4]))
+    cs_sort_key = icu.Collator.createInstance(
+        icu.Locale('cs_CZ.UTF-8')).getSortKey
+    en_sort_key = icu.Collator.createInstance(
+        icu.Locale('en_US.UTF-8')).getSortKey
+
+    def sort_key(entry):
+        return (
+            en_sort_key(entry[0]),
+            cs_sort_key(entry[1]),
+            cs_sort_key(entry[2]),
+            cs_sort_key(entry[3]),
+            cs_sort_key(entry[4]))
+
     entries.sort(key=sort_key)
